9718daa00af0b992c8adef7e99bb4ac2df0fde46
Luke Hayes <lukepurplehayes@gmail.com>
2021-11-24 09:29:28 +0000

    Added working with/open statement.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from bs4 import BeautifulSoup
+import re
 
 filename = "cheatsheet.txt"
 # with open(filename) as fp:
     # soup = BeautifulSoup(fp, 'html.parser')
 
+line_pattern = "r[a-z]{2,7}"
+
 with open(filename, "r") as fh:
     lines = fh.readlines()
-
     for line in lines:
-        print(line)
+        matches = re.match(line_pattern, line)
+        print(matches)
