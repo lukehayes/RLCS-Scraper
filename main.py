83c99edbde12b6619d0a73e9abf2381ac32d85b5
Luke Hayes <lukepurplehayes@gmail.com>
2021-11-25 13:14:12 +0000

    Documentation description now added to FuncDetails class.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ filename = "cheatsheet.txt"
 
 ret_type_pattern = r"^([a-zA-Z0-9]{2,8})(\s\*+)?"
 fn_name_pattern  = r"([A-Z][a-zA-Z]+)"
-description_pattern = r"(\/\/\s)([A-Za-z]+\s?\w*)"
+description_pattern = r"(\/\/\s)(.+)"
 
 details = []
 
@@ -16,21 +16,16 @@ with open(filename, "r") as fh:
     lines = fh.readlines()
     for line in lines:
         type_match = re.search(ret_type_pattern, line)
-        fn_name_match = re.search(fn_name_pattern, line)
-        description_match = re.split(description_pattern, line)
+        # fn_name_match = re.search(fn_name_pattern, line)
+        description_match = [list(l) for l in list(re.findall(description_pattern, line))]
 
-        print(description_match)
+        if description_match:
+            fd = FuncDetail()
+            fd.description = description_match[0][1]
 
-        # fd = FuncDetail("a","b", "c")
-
-        # if type_match is not None:
-            # fd.return_type = type_match.group(0)
-            # fd.fn_name = fn_name_match.group(0)
-            # fd.description = description_match.group(0)
-
-        # details.append(fd)
+            details.append(fd)
 
 
-for detail in details:
-    if detail.return_type != "a":
-        print(f"{detail.return_type} {detail.fn_name}")
+# for d in details:
+    # print(d.description)
+
