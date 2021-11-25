2b86c3be00ea10156bef3b9f93952fbcbd26d2f8
Luke Hayes <lukepurplehayes@gmail.com>
2021-11-25 13:33:20 +0000

    FuncDetail now using @property getter and setters rather than constructor initialzation.

--- RLCS/func_detail.py
+++ RLCS/func_detail.py
@@ -1,8 +1,30 @@
 class FuncDetail(object):
     """Class represents each part of a single line of documentation"""
-    def __init__(self, return_type, fn_name, description):
-        self.return_type = return_type
-        self.fn_name = fn_name
-        self.description = description
+    def __init__(self,):
+        self._return_type = None
+        self._fn_name = None
+        self._description = None
 
+    @property
+    def description(self):
+        return self._description
 
+    @description.setter
+    def description(self, description):
+        self._description = description
+
+    @property
+    def return_type(self):
+        return self._return_type
+
+    @return_type.setter
+    def return_type(self, return_type):
+        self._return_type = return_type
+
+    @property
+    def fn_name(self):
+        return self._fn_name
+
+    @fn_name.setter
+    def fn_name(self, fn_name):
+        self._fn_name = fn_name
