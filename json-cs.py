61825725d31340d1c83a28b95677fdf373ea876f
Luke Hayes <lukepurplehayes@gmail.com>
2021-11-26 10:21:23 +0000

    Added Terminal output helpers.

--- json-cs.py
+++ json-cs.py
@@ -1,6 +1,7 @@
 import json
 from RLCS.func_detail import FuncDetail
 from RLCS.color import Color
+from RLCS.terminal import Terminal
 
 with open("cheatsheet.json") as fh:
     data = json.load(fh)
@@ -8,7 +9,9 @@ with open("cheatsheet.json") as fh:
 cs_json = data["data"]
 
 for line in cs_json:
-    Color.cyan(line['return_type'])
+    Color.magenta(line['return_type'])
     Color.green(line['function_name'])
+    Terminal.arrow();
+    Color.yellow(line['description'])
     Color.end()
 
